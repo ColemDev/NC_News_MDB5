80403d826b3ebcf5d513195c6d6cd9f904c76e50
ColemDev <contactcolem@gmail.com>
2023-02-23 15:53:46 +0000

    testing alt pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-// import AllArticles from './pages/AllArticles';
+import AllArticles from './pages/AllArticles';
 import SingleArticlePage from './pages/SingleArticlePage';
 
 const App = () => {
@@ -9,8 +9,8 @@ const App = () => {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<SingleArticlePage />} />
-        {/* <Route path="/article/test" element={<SingleArticlePage />} /> */}
+        <Route path="/" element={<AllArticles />} />
+        <Route path="/article/test" element={<SingleArticlePage />} />
       </Routes>
       </Router>
   );
